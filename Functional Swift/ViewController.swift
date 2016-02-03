cf6fc6c90c5a21cb632a3dec7d25fc977118d7f5
beeth0ven <beeth0vendev@163.com>
2016-02-03 11:22:11 +0800

    Init

--- Functional Swift/ViewController.swift
+++ Functional Swift/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
+typealias Predicate = Int -> Bool
+
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    func lessThan(maxInt: Int) -> Predicate {
+        return { int in int < maxInt }
+    }
+    
+    func greaterThan(minInt: Int) -> Predicate {
+        return { int in minInt < int }
+    }
+    
+    func and(predicate1 predicate1: Predicate, predicate2: Predicate) -> Predicate {
+        return { int in predicate1(int) && predicate2(int) }
+    }
+    
+    // Not Functional Programing
+    func isBetween(int: Int, minInt: Int, maxInt: Int) -> Bool {
+        let lessThanResult      = int < maxInt
+        let greaterThanResult   = minInt < int
+        return lessThanResult && greaterThanResult
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Functional Programing
+    func isBetween1(int: Int, minInt: Int, maxInt: Int) -> Bool {
+        let lessThanPredicate       = lessThan(maxInt)
+        let greaterThanPredicate    = greaterThan(minInt)
+        let andPredicate            = and(predicate1:lessThanPredicate, predicate2: greaterThanPredicate)
+        return andPredicate(int)
     }
 
-
+    // Functional Programing is Equal to this
+    func isBetween2(int: Int, minInt: Int, maxInt: Int) -> Bool {
+        return and(predicate1:lessThan(maxInt), predicate2: greaterThan(minInt))(int)
+    }
+    
+    // Functional Programing is Equal to this too
+    func isBetween3(int: Int, minInt: Int, maxInt: Int) -> Bool {
+        return { int in
+            { int in int < maxInt }(int) && { int in minInt < int }(int)
+        }(int)
+        
+    }
+    
 }
 
